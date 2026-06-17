15d1f945ea311f81753a82f03dde8bae22090d90
MYWEBSITE-ALIYU <au9006172@gmail.com>
2026-06-18 00:48:27 +0100

    Create protect.js

---
+++ js/protect.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase-config.js";
+
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+export function protectPage() {
+onAuthStateChanged(auth, (user) => {
+if (!user) {
+window.location.href = "login.html";
+}
+});
+}
